58acad3fd719b6332f0634ca31eb86da84a7fe43
alvinwong97 <alvinnwong@gmail.com>
2019-01-06 20:05:52 +1100

    Implemented speech data creation + fixed white noise creation typo

--- data/createData.m
+++ data/createData.m
@@ -2,52 +2,106 @@
 
 close all; clear all; clc
 
-%% PATHS
-PATH = 'new_data/';
-
-%% TOGGLES/SETTINGS
+%% TOGGLES
 % Toggles
 % speech - toggles between speech (1) and white noise (0)
-% flight - toggles between flight (1) and static (0) files
 % vary - toggles between varying snr
-speech = 0;
-flight = 0;
-vary = 0;
+speech = 1;
+snrRange = 0;
 
-snrs = [0 -10];
+%% SETTINGS
+snrs = [-10];
 motorSpeed = 50;
 
-%% SOURCE GENERATION
-mkdir(PATH)
-if flight
-    if speech
-
-    else
-
-    end
+%% PATHS
+PATH = 'new_data/';
+if speech 
+    PATH_SOURCE = 'dev_static/speech/';
+    PATH_NEW_DATA = [PATH 'speech/'];
 else
-    if speech
-        
-    else
-        PATH_SOURCE = 'dev_static/audio/';
-        whiteNoiseFiles = dir('dev_static/audio/*.wav');
+    PATH_SOURCE = 'dev_static/audio/';
+    PATH_NEW_DATA = [PATH 'broadband/'];
+end
+
+%% FOLDER CREATION
+mkdir(PATH)
+mkdir(PATH_NEW_DATA)
+
+%% 
+
+if speech
+    c = 34300;
+    % The (X,Y,Z) positions of the 8 microphones, in meters, 
+    % with respect to the center of the array are given below:
+    micPos = 100*[0.0420   0.0615  -0.0410;  % mic 1
+             -0.0420   0.0615   0.0410;  % mic 2
+             -0.0615   0.0420  -0.0410;  % mic 3
+             -0.0615  -0.0420   0.0410;  % mic 4
+             -0.0420  -0.0615  -0.0410;  % mic 5
+              0.0420  -0.0615   0.0410;  % mic 6
+              0.0615  -0.0420  -0.0410;  % mic 7
+	          0.0615   0.0420   0.0410]; % mic 8
+    delay = zeros(1,8);
+    for spkrNum = 1:10
+        PATH_SPEAKER = [PATH_SOURCE 'Speaker' int2str(spkrNum) '/'];
+        speechFiles = dir([PATH_SPEAKER '*.WAV']);
         for i = 1:length(snrs)
-            filenum = 1;
-            FILE_PATH = [PATH int2str(snrs(i)) '/'];
-            mkdir(FILE_PATH)
-            for j = 1:length(whiteNoiseFiles)
-                PATH_AUDIO = [PATH_SOURCE whiteNoiseFiles(j).name];
+            fileNum = 1;
+            PATH_FILE = [PATH_NEW_DATA int2str(snrs(i)) '/'];
+            mkdir(PATH_FILE)
+            for j = 1:length(speechFiles)
+                PATH_AUDIO = [PATH_SPEAKER speechFiles(j).name];
                 [y, fs] = audioread(PATH_AUDIO);
+                % distance in cm
+                theta = 40;
+                phi = -45;
+                d = 1000;
+                cart = [d*cos(theta*pi/180)*cos(phi*pi/180) d*sin(theta*pi/180)*cos(phi*pi/180) d*sin(phi*pi/180)];
+                for k = 1:8
+                    delay(k) = norm(cart - micPos(k,:))/c;
+                end
+                delaySampled = round(delay*16*fs);
+                yResampled = resample(y,16,1);
+                % Largest delay shift
+                maxDelay = max(delaySampled);
+                minDelay = min(delaySampled);
+                for k = 1:8
+                    Y_temp = [zeros(delaySampled(k),1); yResampled];
+                    delayedY(:,k) = Y_temp(maxDelay:length(yResampled)+minDelay);
+                end
+                y = resample(delayedY,1,16);
                 noise = mixMotorNoise(y,motorSpeed);
                 noise = (noise/norm(rms(noise)))*(norm(rms(y))/10.0^(0.05*snrs(i)));
                 mixed = y + noise;
                 while 1
-                    filename = [FILE_PATH int2str(filenum) '.wav'];
-                    filenum = filenum + 1;
-                    if ~isfile([filename])
-                         audiowrite(filename,y,fs);
-                        break
+                    fileName = [PATH_FILE int2str(fileNum) '.wav'];
+                    fileNum = fileNum + 1;
+                    if ~isfile(fileName)
+                        audiowrite(fileName,mixed,fs);
+                    break
                     end
+                end
+            end
+        end
+    end
+else
+    whiteNoiseFiles = dir('dev_static/audio/*.wav');
+    for i = 1:length(snrs)
+        fileNum = 1;
+        PATH_FILE = [PATH_NEW_DATA int2str(snrs(i)) '/'];
+        mkdir(PATH_FILE)
+        for j = 1:length(whiteNoiseFiles)
+            PATH_AUDIO = [PATH_SOURCE whiteNoiseFiles(j).name];
+            [y, fs] = audioread(PATH_AUDIO);
+            noise = mixMotorNoise(y,motorSpeed);
+            noise = (noise/norm(rms(noise)))*(norm(rms(y))/10.0^(0.05*snrs(i)));
+            mixed = y + noise;
+            while 1
+                fileName = [PATH_FILE int2str(fileNum) '.wav'];
+                fileNum = fileNum + 1;
+                if ~isfile(fileName)
+                     audiowrite(fileName,mixed,fs);
+                    break
                 end
             end
         end
