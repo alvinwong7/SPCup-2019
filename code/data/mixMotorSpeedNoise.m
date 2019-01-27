ef583eb231aa9de7d147628f8382ecded73d3cf8
Alvin <alvinnwong@gmail.com>
2019-01-28 00:46:14 +1100

    Fixed changing motor speed noise

--- code/data/mixMotorSpeedNoise.m
+++ code/data/mixMotorSpeedNoise.m
@@ -17,11 +17,19 @@ end
 samples = floor(length(x)/5);
 varyNoise = [];
 for speed = 50:10:90
-    noise = zeros(samples, col);
+    if speed ~= 90
+        noise = zeros(samples, col);
+    else
+        noise = zeros((r+length(x)) - (r+samples*(speed/10-5)),col);
+    end
     for k = 1:4
         PATH_AUDIO = ['individual_motors_cut/Motor' num2str(k) '_' num2str(speed) '.wav'];
         [y, fs] = audioread(PATH_AUDIO);
-        noise = noise + y(r+samples*(speed/10-5):r+samples*(speed/10-4)-1,:);
+        if speed ~= 90
+            noise = noise + y(r+samples*(speed/10-5):r+samples*(speed/10-4)-1,:);
+        else
+            noise = noise + y(r+samples*(speed/10-5):r+(length(x)-1),:);
+        end
     end
     varyNoise = [varyNoise; noise];
 end
