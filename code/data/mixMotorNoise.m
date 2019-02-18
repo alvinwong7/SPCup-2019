1c7fd359b85414555e69d26760bf5df98f673516
Alvin <alvinnwong@gmail.com>
2019-02-18 15:29:26 +1100

    Changing mixing for motor speed 70 to use all motor file

--- code/data/mixMotorNoise.m
+++ code/data/mixMotorNoise.m
@@ -2,21 +2,29 @@
 
 function noise = mixMotorNoise(x, speed)
 % I'll optimise this later
-lowest = 1000000000000000;
-for k = 1:4
-    PATH_AUDIO = ['individual_motors_cut/Motor' num2str(k) '_' num2str(speed) '.wav'];
-    [y, fs] = audioread(PATH_AUDIO);
-    if lowest > length(y)
-        lowest = length(y);
-        r = floor(1 + (length(y)-length(x)-1).*rand(1,1));
+if speed ~= 70
+    lowest = 1000000000000000;
+    for k = 1:4
+        PATH_AUDIO = ['individual_motors_cut/Motor' num2str(k) '_' num2str(speed) '.wav'];
+        [y, fs] = audioread(PATH_AUDIO);
+        if lowest > length(y)
+            lowest = length(y);
+            r = floor(1 + (length(y)-length(x)-1).*rand(1,1));
+        end
     end
-end
-[row, col] = size(x);
-noise = zeros(row, col);
-for k = 1:4
-    PATH_AUDIO = ['individual_motors_cut/Motor' num2str(k) '_' num2str(speed) '.wav'];
+    [row, col] = size(x);
+    noise = zeros(row, col);
+    for k = 1:4
+        PATH_AUDIO = ['individual_motors_cut/Motor' num2str(k) '_' num2str(speed) '.wav'];
+        [y, fs] = audioread(PATH_AUDIO);
+        noise = noise + y(r:r+length(x)-1,:);
+    end
+else
+    PATH_AUDIO = ['individual_motors_cut/allMotors_70.wav'];
     [y, fs] = audioread(PATH_AUDIO);
+    r = floor(1 + (length(y)-length(x)-1).*rand(1,1));
+    [row, col] = size(x);
+    noise = zeros(row, col);
     noise = noise + y(r:r+length(x)-1,:);
 end
-
 end
