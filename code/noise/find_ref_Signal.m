3dde490ca2f607c5ddc007941fd0d68d3de06c5a
Nors Cheng <cerulean2014@hotmail.com>
2019-02-21 22:29:34 +1100

    Updated with more optional input terms

--- code/noise/find_ref_Signal.m
+++ code/noise/find_ref_Signal.m
@@ -1,21 +1,24 @@
-function [refSignal] = find_ref_Signal(speeds)
-%this function gives you the rotor noise signal according to estimated
-%rotor speed.
+function [refSignal] = find_ref_Signal(speeds, motor_nums, fs, ref_time_length)
+% This function gives you the rotor noise signal according to estimated rotor speed.
+% motor_nums should be an array of any sequences between 1:4
+% fs is sampling frequency, ref_time_length is the time length you want to take as Wiener reference
 
+    refSignal = zeros(8,fs*ref_time_length);
     for i = 1:length(speeds)
         lower_speed = floor(speeds(i)/10)*10;
         higher_speed = lower_speed+10;
-        lower_index = (speeds(i)-floor(speeds(i)/10)*10)/10;
-        higher_index = 1-lower_speed;
+        lower_weight = (speeds(i)-floor(speeds(i)/10)*10)/10;
+        higher_weight = 1-lower_speed;
         
-        lower_MotorNoiseFile = ["..\individual_motors_recordings\" + "Motor1_" + ...
-        num2str(lower_speed) + ".wav"];
-        [m1,~] = audioread(lower_MotorNoiseFile);
-    
-        higher_MotorNoiseFile = ["..\individual_motors_recordings\" + "Motor1_" + ...
-        num2str(higher_speed) + ".wav"];
-        [m2,~] = audioread(higher_MotorNoiseFile);
-        refSignal = m1*lower_index + m2*higher_index; %weighted sum of the motor noise
+        for j = motor_nums
+            lower_MotorNoiseFile = ["..\individual_motors_recordings\" + "Motor" + num2str(j) + "_" + num2str(lower_speed) + ".wav"];
+            [m1,~] = audioread(lower_MotorNoiseFile);
+
+            higher_MotorNoiseFile = ["..\individual_motors_recordings\" + "Motor" + num2str(j) + "_" + num2str(higher_speed) + ".wav"];
+            [m2,~] = audioread(higher_MotorNoiseFile);
+            refSignal(i,:) = refSignal(i,:) + m1*lower_weight + m2*higher_weight; %weighted sum of the motor noise
+        end
+        
     end
 end
 
