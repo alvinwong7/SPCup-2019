c74276c68b8fc1c45701b6f06804fd1a3fb18d27
Nors Cheng <cerulean2014@hotmail.com>
2019-03-05 15:23:10 +1100

    Update with more 'feasible'

--- code/noise/GuessSpeed.m
+++ code/noise/GuessSpeed.m
@@ -1,37 +1,25 @@
- function [speeds] = GuessSpeed(InputSignals, fs)
+function [speeds] = GuessSpeed(InputSignals, fs)
     % input: InputSignals: input speech+motor noise signal
     % output: speeds: gives your the estimated rotor speed for each channal
  
         %This function tries to predict the closet motor speed that can be
         %used from given motor noise data sets.
         n = size(InputSignals,2);
-        MagPeakPositions = zeros(1,n);
+        speeds = zeros(1,n);
         for k = 1:n
             mags = abs(fft(InputSignals(:,k)));
-            MagPeakPositions(k) = find(max(mags) == mags,1);
-        end
-        speeds = MagPeakPositions/size(InputSignals,1)*fs;
-        
-        % We want the guessed speed to always be in the range of [50,90], this still needs more effective methods!
-        for c = 1:length(speeds)
-            if speeds(c) > 50 && speeds(c) < 90
-                continue;
-            else
-                while speeds(c) <= 45
-                    speeds(c) = speeds(c) * 2;
-                    continue;
-                end
-                while speeds(c) >= 100
-                    speeds(c) = speeds(c) / 2;
-                end
-                if speeds(c) > 45
-                    speeds(c) = 50;
-                else
-                    % i.e. [90, 100)
-                    speeds(c) = 90;
+            sorted_mags = sort(mags, 'descend');
+            for counter = 1:length(sorted_mags)
+                peak = sorted_mags(counter);
+                index = find(mags == peak, 1);
+                speed = index/size(InputSignals,1)*fs;
+                if speed >= 50 && speed <= 90
+                    break;
                 end
             end
+            speeds(k) = speed;
         end
-        %speeds = round(speeds/10)*10;
-        %PredictedSpeed = mode(speeds);
+        
+        % We want the guessed speed to always be in the range of [50,90], this still needs more effective methods!
+
     end
