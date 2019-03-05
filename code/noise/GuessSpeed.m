f40b8d9c3cb2cd32027ae4f55d63ea508c263604
Nors Cheng <cerulean2014@hotmail.com>
2019-03-05 15:55:53 +1100

    Updated to avoid errors

--- code/noise/GuessSpeed.m
+++ code/noise/GuessSpeed.m
@@ -20,6 +20,24 @@ function [speeds] = GuessSpeed(InputSignals, fs)
             speeds(k) = speed;
         end
         
+        ten_digits = round(speeds/10);
+        most = mode(ten_digits * 10);
+        for i = 1:length(speeds)
+            if abs(speeds(i) - most) >= 20
+                % This is an error estimation, re do it again
+                mags = abs(fft(InputSignals(:,k)));
+                sorted_mags = sort(mags, 'descend');
+                for counter = 1:length(sorted_mags)
+                    peak = sorted_mags(counter);
+                    index = find(mags == peak, 1);
+                    speed = index/size(InputSignals,1)*fs;
+                    if abs(speed - most) < 20
+                        break;
+                    end
+                end
+            end
+        end
+        
         % We want the guessed speed to always be in the range of [50,90], this still needs more effective methods!
 
     end
